86d187b0447e92cd49c4024d3022e7b3d7a5ac82
Cassandra Colon <thatcass@thatcass.myfiosgateway.com>
2021-04-14 10:52:28 -0400

    first iteration

--- src/views/Modura.vue
+++ src/views/Modura.vue
@@ -10,19 +10,104 @@
       </div>
       <h2 class="title is-2 has-text-centered">An eCommerce Prototype</h2>
     </div>
-    <div class="columns is-centered">
+
+    <div class="columns is-centered is-vcentered px-3">
+
         <div class="column is-one-third pt-6">
-          <figure class="">
-              <img src="../assets/modura/ModuraLightBackground.png" alt="MODURA">
+          <h2 class="title is-3">The Brief:</h2>
+          <p>
+            Modura, an e-Commerce clothing brand, is designed for trendy men and women between 18 - 25 years old. 
+            These individuals are early in their careers and are looking for an affordable solution for business casual clothing to wear to their jobs in urban areas. 
+            They are fashion-forward, and conscious of trends. They want to dress for the jobs they want without appearing overdressed.
+          </p>
+        </div>
+
+        <div class="column is-one-third py-6 my-6">
+          <figure class="image is-1by2 screenimg">
+              <img src="../assets/modura/Modura3.png" alt="MODURA">
           </figure>
         </div>
-        <div class="column is-one-third pt-6">
-          <figure class="">
-              <img src="../assets/modura/ModuraLightBackground.png" alt="MODURA">
-          </figure>
+    </div>
+
+    <div class="columns is-centered py-6 my-6"> 
+      <div class="column is-three-quarters ">
+        <div class="box has-background-primary">
+          <div class="content center"> 
+            <h2 class="title is-3">Goals:</h2>
+            <li>Complete logo design, wireframing, and basic prototype in 10 hours total</li>
+            <li>Evoke a sense of effortless business readniess</li>
+            <li>Create Login, Account Details, and Item Details pages</li>
+          </div>
         </div>
       </div>
+    </div>
+
+    <div class="columns is-centered py-6">
+      <div class='column is-half'> 
+          <h2 class="title is-3 content center">Logo Design:</h2>
+          <p>
+            Everything begins on paper as I iterate through logos that combine elements of elegance and business fashion.  
+            I had many concepts that encorporated a tie into the logo, but after further consideration, ties gave off a more rigid, less youthful feel than Modura’s target audience. 
+            The final logo was clean, with simple edges, and had a business suit color scheme without feeling too imposing.
+          </p>
+        </div>
+    </div>
+
+    <div class="columns is-centered px-3">
+      <div class='column is-one-third'>
+          <figure class="image screenimg">
+              <img src="../assets/modura/ModuraLight.png" alt="MODURA">
+          </figure>
+      </div>
+      <div class='column is-one-third'>
+          <figure class="image screenimg">
+              <img src="../assets/modura/modura.jpg" alt="MODURA">
+          </figure>
+      </div>
+      <div class='column is-one-third'>
+          <figure class="image screenimg">
+              <img src="../assets/modura/ModuraDark.png" alt="MODURA">
+          </figure>
+      </div>
+    </div>
+
+
+
+    <div class="columns is-centered px-3 pt-6 mt-6">
+        <div class='column is-half'> 
+            <h2 class="title is-3 content center">Page Mockups:</h2>
+            <p>
+              For this 10 hour design challenge I first focused on creating the account screen, the users first introduction to Modura’s application. 
+              Additionally, creating the Account Settings page and Item Selection page allowed me to explore different stylistic ways to convey Modura’s brand.
+            </p>
+          </div>
+      </div>
     
+    <!-- <div class="columns is-centered px-3">
+      <div class='column is-one-third'>
+          <figure class="image screenimg">
+              <img src="../assets/modura/Modura1.png" alt="MODURA1">
+          </figure>
+      </div>
+      <div class='column is-one-third'>
+          <figure class="image screenimg">
+              <img src="../assets/modura/Modura2.png" alt="MODURA2">
+          </figure>
+      </div>
+      <div class='column is-one-third'>
+          <figure class="image screenimg">
+              <img src="../assets/modura/Modura3.png" alt="MODURA3">
+          </figure>
+      </div> 
+    </div>-->
+   
+    <div class="columns is-centered px-3">
+      <div class='column is-three-quarters'>
+          <figure class="image">
+              <img src="../assets/modura/PageMockups2.png" alt="">
+          </figure>
+      </div>
+    </div>
 
 </div>
     
